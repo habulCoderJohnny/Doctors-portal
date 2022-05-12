1a6c5d1b2385dfbaa48d935fd7b6ca19a17cb538
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-12 19:14:41 +0600

    menu-item setup dynamically & anchor to link.

--- src/PAGES/SHARED/Navbar.js
+++ src/PAGES/SHARED/Navbar.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
+    const menuItems = 
+    <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/appointment">Appointment</Link></li>
+        <li><Link to="/review">Review</Link></li>
+        <li><Link to="/who-we-are">Contact Us</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/login">Login</Link></li>
+    </>
+    return(
         <div class="navbar bg-base-100">
             <div class="navbar-start">
                 <div class="dropdown">
@@ -9,24 +19,14 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
-                        <li><a>Appointment</a></li>
-                        <li><a>Review</a></li>
-                        <li><a>Contact Us</a></li>
-                        <li><a>About</a></li>
-                        <li><a>Login</a></li>
+                        {menuItems}
                     </ul>
                 </div>
                 <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
             </div>
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal p-0">
-                    <li><a>Home</a></li>
-                    <li><a>Appointment</a></li>
-                    <li><a>Review</a></li>
-                    <li><a>Contact Us</a></li>
-                    <li><a>About</a></li>
-                    <li><a>Login</a></li>
+                    {menuItems}
                 </ul>
             </div>
             <div class="navbar-end">
