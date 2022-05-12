b000429521d5ea805721b86a1417e231e61f2ca0
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-12 18:58:55 +0600

    setup routes & created daisyui navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./PAGES/About/About";
+import Navbar from "./PAGES/SHARED/Navbar";
+
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<home/>}></Route>
+        <Route path="" element={<About/>}></Route>
+      </Routes>
+
   
     </div>
   );
